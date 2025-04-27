f87c3f5fdc917e5e9840e6f3b1e4eb174cd019e7
fathimahabeeb222 <134144650+fathimahabeeb222@users.noreply.github.com>
2025-04-27 20:53:04 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,69 @@
+
+
+for(var i=0;i<7;i++)
+{
+    document.querySelectorAll(".drum")[i].addEventListener("click", play);
+   
+}
+function play(){
+    
+    common(this.innerHTML);
+    animate(this.innerHTML);
+}
+
+document.addEventListener("keydown",function (event)
+{
+    common(event.key);
+    animate(event.key);
+})
+
+function common(value)
+{
+    switch (value) {
+        case "w":
+            var crash= new Audio('sounds/crash.mp3');
+            crash.play();
+            break;
+        case "a":
+            var kick= new Audio('sounds/kick-bass.mp3');
+            kick.play();
+            break;
+        case "s":
+            var snare= new Audio('sounds/snare.mp3');
+            snare.play();
+            break;
+        case "d":
+            var tom1= new Audio('sounds/tom-1.mp3');
+            tom1.play();
+            break;
+        case "j":
+            var tom2= new Audio('sounds/tom-2.mp3');
+            tom2.play();
+            break;
+        case "k":
+            var tom3= new Audio('sounds/tom-3.mp3');
+            tom3.play();
+            break;
+        case "l":
+            var tom4= new Audio('sounds/tom-4.mp3');
+            tom4.play();
+            break;
+    
+        default:console.log(this.innerHTML);
+           
+       }
+}
+
+function animate(value)
+{
+document.querySelector("."+value).classList.add("pressed");
+setTimeout(function (){
+    document.querySelector("."+value).classList.remove("pressed");
+},100)
+}
+
+
+
+
+
+
