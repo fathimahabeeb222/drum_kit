0f74e5d5599fc10d97af4f3fd08d3efa96f6835b
fathimahabeeb222 <134144650+fathimahabeeb222@users.noreply.github.com>
2025-09-25 10:38:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -24,27 +24,27 @@ function common(value)
             var crash= new Audio('crash.mp3');
             crash.play();
             break;
-        case "a":
+        case a:
             var kick= new Audio('kick-bass.mp3');
             kick.play();
             break;
-        case "s":
+        case s :
             var snare= new Audio('snare.mp3');
             snare.play();
             break;
-        case "d":
+        case d :
             var tom1= new Audio('tom-1.mp3');
             tom1.play();
             break;
-        case "j":
+        case j :
             var tom2= new Audio('tom-2.mp3');
             tom2.play();
             break;
-        case "k":
+        case k :
             var tom3= new Audio('tom-3.mp3');
             tom3.play();
             break;
-        case "l":
+        case l :
             var tom4= new Audio('tom-4.mp3');
             tom4.play();
             break;
